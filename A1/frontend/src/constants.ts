0f35daa4b7c90ad4f45412804cd15afa80ab746c
912-Dunca-GeorgeRaulMichael <george.dunca@stud.ubbcluj.ro>
2023-04-03 10:55:01 +0300

    .

--- A1/frontend/src/constants.ts
+++ A1/frontend/src/constants.ts
@@ -1,5 +1,5 @@
 const PROD_BACKEND_API_URL = "/";
-const DEV_BACKEND_API_URL = "172.31.4.129";
+const DEV_BACKEND_API_URL = "13.50.246.37";
 
 export const BACKEND_API_URL =
 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
